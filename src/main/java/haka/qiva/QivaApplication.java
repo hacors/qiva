f1733a7a92c292b7ed2dae1bc17f4bd75c1b3bda
mubadao <mubadao@pdd.com>
2022-07-31 01:26:58 +0800

    Initial commit

---
+++ src/main/java/haka/qiva/QivaApplication.java
@@ -0,0 +1,13 @@
+package haka.qiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QivaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QivaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/haka/qiva/QivaApplicationTests.java
@@ -0,0 +1,13 @@
+package haka.qiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QivaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
